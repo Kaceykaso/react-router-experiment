5bde5170a380834a4495c461299291f521a6ef7b
Kaso <mackovision@gmail.com>
2024-06-11 12:48:25 -0700

    Update main.jsx
    Cleaning up

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
-import Root, { loader as rootLoader, action as rootAction } from './routes/root';
 import ErrorPage from './error-page';
+import Root, { loader as rootLoader, action as rootAction } from './routes/root';
+import Index from './routes/index';
 import Contact, { loader as contactLoader } from './routes/contact';
 import EditContact, { action as editAction } from './routes/edit';
 import { action as destroyAction } from './routes/destroy';
-import Index from './routes/index';
 
 const router = createBrowserRouter([
   {
